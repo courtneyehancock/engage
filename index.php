4803ea15a278e6add17d594157552b5ce8347821
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-08-07 16:10:48 -0400

    Edit blog code

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 <div class="main-content">
   <div class="internal-div">
     <?php dynamic_sidebar('banner-home'); ?>
-
-    <hr />
+    <h1 class="text-center">CHECK HERE FOR ALL FLAWARN UPDATES</h1>
     <div class="container d-flex p-4 justify-content-center align-content-center align-items-end">
-      <h1>CHECK HERE FOR ALL FLAWARN UPDATES</h1>
   <h2 class="pr-3">Looking for something specific?</h2><form role="search" method="get" id="searchform" class="searchform" action="https://pwd.aa.ufl.edu/">
       <div>
         <label class="screen-reader-text" for="s">Search for:</label>
