98d0a73da3cf05e77fc98286ea0f50c8a360a75b
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-15 16:09:27 -0400

    Edit active class

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 <div class="main-nav">
   <ul class="menu">
 <li title="home"><a href="https://pwd.aa.ufl.edu/cdprs/home" class="home">home</a></li>
-<li title="search"><a href="https://pwd.aa.ufl.edu/cdprs/about" class="search">about</a></li>
+<li title="search"><a href="https://pwd.aa.ufl.edu/cdprs/about" class="active search">about</a></li>
 <li title="pencil"><a href="https://pwd.aa.ufl.edu/cdprs/agenda" class="pencil">agenda</a></li>
 <li title="about"><a href="#" class="about">speakers</a></li>
 <li title="archive"><a href="#" class="archive">posters</a></li>
