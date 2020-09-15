0c352d63e99cb0ceb3ebeb1a8cb2bdf1efaf91a2
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-15 15:51:58 -0400

    update pages

--- header.php
+++ header.php
@@ -50,11 +50,11 @@
 <div class="main-nav">
   <ul class="menu">
 <li title="home"><a href="#" class="home">home</a></li>
-<li title="search"><a href="#" class="search">search</a></li>
-<li title="pencil"><a href="#" class="pencil">pencil</a></li>
-<li title="about"><a href="#" class="active about">about</a></li>
-<li title="archive"><a href="#" class="archive">archive</a></li>
-<li title="contact"><a href="#" class="contact">contact</a></li>
+<li title="search"><a href="#" class="search">about</a></li>
+<li title="pencil"><a href="#" class="pencil">agenda</a></li>
+<li title="about"><a href="#" class="about">speakers</a></li>
+<li title="archive"><a href="#" class="archive">posters</a></li>
+<li title="contact"><a href="#" class="contact">submit</a></li>
 </ul>
 </div>
 
