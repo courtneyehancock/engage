e676033b8ed0ff18c85dcccaafebe8b6f78226cd
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-16 13:42:03 -0400

    Remove JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,1 @@
-$(document).ready(function() {
 
-  // Active link switching
-  $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
-
-    scrollLink.each(function() {
-
-      var sectionOffset = $(this.hash).offset().top - 20;
-
-      if ( sectionOffset <= scrollbarLocation ) {
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
-      }
-    })
-
-  })
-
-})
