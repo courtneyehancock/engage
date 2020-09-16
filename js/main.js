be97b038ab90948e19bda5c0562606fc92664aa0
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-09-16 13:11:24 -0400

    Remove unnecessary js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
-var links = document.querySelectorAll("#navbar li a");
-  for (const link of links) {
-    link.addEventListener("click", (e) => {
-    var target = e.target;
-
-    links.forEach(function(currentLink) {
-        currentLink.classList.remove('active');
-    });
-
-      // if target already had .active. remove it. Otherwise, add it
-      target.classList.toggle('active');
-    })
-}
-
 $(document).ready(function() {
            $('li').click(function() {
-               $('li.menu-item.active').removeClass("active");
-               $(this).addClass("active");
+               $('li.menu-item.active').removeClass('active');
+               $(this).addClass('active');
            });
-       }); 
+       });
