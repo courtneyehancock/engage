1615bdfd2d77f2b286178ef1eb86118939e12d31
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2020-10-21 11:31:16 -0400

    Get rid of the excerpt from blog posts

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
           <div class="col-lg-3 posts">
             <?php the_post_thumbnail('medium'); ?>
             <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <?php the_excerpt(); ?>
+            <!--<?php the_excerpt(); ?>-->
             <?php
               $archive_year = get_the_time('Y');
               $archive_month = get_the_time('m');
