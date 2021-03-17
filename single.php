f4c87b144fb3a3b043a3584570cf6f9acd6723c1
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-03-17 10:09:34 -0400

    Edit page styles and edit single post headers

--- single.php
+++ single.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
 <div class="main-content pt-5 pb-5">
+  <div class="hero-image" style="background-color:#001b3d; background-image: linear-gradient(to bottom, rgba(0, 27, 61, 1), rgba(245, 246, 252, 0.52)), url('https://flawarn.pwd.aa.ufl.edu/wp-content/uploads/sites/12/2020/12/download.jpg'); height:300px; background-position: 50% 25%; background-size:cover;">
+  </div>
   <div class="container mt-5">
   <?php
     if(have_posts()){
