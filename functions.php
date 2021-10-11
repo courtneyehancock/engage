2352938e0806aae11b7e1557e4df91fdd075c32e
Hancock <courtneyehanco1k-dce@TSS-DL-001A.local>
2021-10-11 14:27:54 -0400

    Edit functions of menu

--- functions.php
+++ functions.php
@@ -1,35 +1,5 @@
 <?php
-  /*-------------------------------------
 
-  Add MENUS
-
-  -----------------------------------------*/
-  add_theme_support('menus');
-
-  register_nav_menus(array(
-    'top-menu' => ('Top Menu', 'theme')
-  ));
-
-  /*function add_class_li($classes,$item,$args){
-    if(isset($args->li_class)) {
-      $classes[] = $args->li_class;
-    }
-    if(isset($args->active_class) && in_array('current-menu-item',$classes)){
-      $classes[] = $args->active_class;
-    }
-    return $classes;
-  }
-
-  add_filter( 'nav_menu_css_class', 'add_class_li', 10, 3 );
-
-  function add_anchor_class($attr,$item,$args){
-    if(isset($args->a_class)){
-      $attr['class'] = $args->a_class;
-    }
-    return $attr;
-  }
-
-  add_filter( 'nav_menu_link_attributes', 'add_anchor_class', 10, 3 );*/
   /*-------------------------------------
 
   Adds style sheet and JavaScript files
@@ -148,14 +118,42 @@
 
   add_action('widgets_init', 'blank_widgets_init');
 
-  //Custom Menus
+  /*-------------------------------------
+
+  Add MENUS
+
+  -----------------------------------------*/
+  add_theme_support('menus');
+  
   function custom_menus(){
     register_nav_menus(array(
-    //  'header-menu' => __('Header Menu'),
+      'top-menu' => __('Top Menu', 'theme'),
       'footer-menu' => __('Footer Menu')
     ));
   }
+
   add_action('init', 'custom_menus');
+
+  function add_class_li($classes,$item,$args){
+    if(isset($args->li_class)) {
+      $classes[] = $args->li_class;
+    }
+    if(isset($args->active_class) && in_array('current-menu-item',$classes)){
+      $classes[] = $args->active_class;
+    }
+    return $classes;
+  }
+
+  add_filter( 'nav_menu_css_class', 'add_class_li', 10, 3 );
+
+  function add_anchor_class($attr,$item,$args){
+    if(isset($args->a_class)){
+      $attr['class'] = $args->a_class;
+    }
+    return $attr;
+  }
+
+  add_filter( 'nav_menu_link_attributes', 'add_anchor_class', 10, 3 );
 
   //Logo in the header
   add_theme_support( 'custom-header', array(
